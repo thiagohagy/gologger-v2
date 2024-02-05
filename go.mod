b62a62ec1acd2ccd040c893f4efd17b24f507a64
Thiago A. Hagy <thiago.hagy@intelbras.com.br>
2024-02-05 11:46:22 -0300

    Test commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module git.intelbras.com.br/tmr/moovsec/libs/gologger
+
+go 1.21.6
---
+++ main.go
@@ -0,0 +1,7 @@
+package gologger
+
+import "fmt"
+
+func SecretProcess() {
+	fmt.Println("Running the logger process!")
+}
